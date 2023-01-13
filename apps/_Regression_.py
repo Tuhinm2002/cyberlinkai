acf1242fed30105bd34cb98a098ec7070b928e64
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2023-01-13 12:11:04 +0530

    Update _Regression_.py

--- apps/_Regression_.py
+++ apps/_Regression_.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import tensorflow as tf 
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression,LogisticRegression,Lasso,Ridge
 from io import StringIO
